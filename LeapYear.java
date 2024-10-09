5affa867ce93e2b9eccb1c987942f4ffd9ae1d16
SrishtiNehra <srishtinehra1@gmail.com>
2024-10-09 14:40:42 +0530

    [Srishti] LeapYear

---
+++ LeapYear.java
@@ -0,0 +1,35 @@
+//leap year code
+
+import java.util.*;
+public class LeapYear{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+
+
+if(n%4==0){
+if(n%100==0){
+if(n%400 ==0){
+System.out.println("Leap Year");
+}
+else{
+System.out.println("Not a leap Year");
+}
+}
+else{
+System.out.println("Leap Year");
+}
+
+
+
+}
+else{
+System.out.println("Not a leap Year");
+}
+
+
+}
+
+}
+
+
