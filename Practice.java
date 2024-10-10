37c3d5e0330c6719a798ad89a83c0c49f277eee9
SrishtiNehra <srishtinehra1@gmail.com>
2024-10-10 10:39:26 +0530

    [Srishti]ADD : Added \n,\t,\r

---
+++ Practice.java
@@ -0,0 +1,18 @@
+
+//here \r will not print the rest of the line
+
+public class Practice{
+public static void main(String[] args){
+String str = "Hello  \r World srishti Nehra";
+String str1 = "Hello\tWorld";
+String str2 = "Hello\nWorld";
+System.out.println(str);
+System.out.println(str1);
+System.out.println(str2);
+}
+}
+
+
+
+//topics
+//\r   \t   \n
